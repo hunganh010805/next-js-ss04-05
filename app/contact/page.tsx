55eef6eb33260e9a465fdab73294fd3ec14aa756
hunganh010805 <teugiatre123@gmail.com>
2024-08-16 15:29:44 +0700

    ss04-05

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+
 export default function page() {
   return (
-    <div> 
-       Bai 1: Đây là trang chủ
+    <div> Bai1 :
+      Day la tranng lien he
+
     </div>
   )
 }
--- app/navigation/home/page.tsx
+++ app/navigation/home/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+
 export default function page() {
   return (
-    <div> 
-       Bai 1: Đây là trang chủ
+    <div>
+      Home Pages
     </div>
   )
 }
